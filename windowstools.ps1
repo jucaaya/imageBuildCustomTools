35a9f9c195bb7e22fa898e68cfb50b91e5708f0c
Julius Ca-aya <julius.ca-aya@3Cloud-KLFW3HH25Q.local>
2024-10-16 23:28:55 +0800

    Update custom script

--- windowstools.ps1
+++ windowstools.ps1
@@ -3,7 +3,7 @@ Write-Output "Starting installation of Azure CLI, JQ, .NET SDK, and Terraform us
 # 1. Install Scoop
 
 if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
-    Write-Output "Scoop not found. Installing Chocolatey..."
+    Write-Output "Scoop not found. Installing Scoop..."
     # Install Scoop as Admin
     iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 } else {
@@ -13,7 +13,14 @@ if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
 # 2. Install Git for Scoop Update
 Write-Output "Installing Git..."
 scoop install git
-git --version
+if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+    Write-Output "git not found...."
+
+} else {
+    Write-Output "Git installed successfully."
+    git --version
+}
+
 
 # 3. Update Scoop to ensure the latest package information
 Write-Output "Updating Scoop..."
@@ -22,25 +29,58 @@ scoop update
 # 4. Install Azure CLI
 Write-Output "Installing Azure CLI..."
 scoop install azure-cli
-az --version
-Write-Output "Azure CLI installed successfully."
+if (-not (Get-Command az -ErrorAction SilentlyContinue)) {
+    Write-Output "Azure CLI not found...."
+
+} else {
+    Write-Output "Azure CLI installed successfully."
+    az --version
+}
 
 # 5. Install JQ
 Write-Output "Installing JQ..."
 scoop install jq
-jq --version
-Write-Output "JQ installed successfully."
+if (-not (Get-Command jq -ErrorAction SilentlyContinue)) {
+    Write-Output "jq not found...."
 
-# 6. Install .NET SDK
+} else {
+    Write-Output "JQ installed successfully."
+    jq --version
+}
+
+# 6. Install JID
+Write-Output "Installing JID..."
+scoop install jid
+if (-not (Get-Command jid -ErrorAction SilentlyContinue)) {
+    Write-Output "jid not found...."
+
+} else {
+    Write-Output "JID installed successfully."
+    jid --version
+}
+
+# 7. Install .NET SDK
 Write-Output "Installing .NET SDK..."
 scoop install dotnet-sdk
-dotnet --info
-Write-Output ".NET SDK installed successfully."
+if (-not (Get-Command dotnet -ErrorAction SilentlyContinue)) {
+    Write-Output "dotnet not found...."
 
-# 7. Install Terraform
+} else {
+    Write-Output ".NET SDK installed successfully."
+    dotnet --info
+}
+
+
+# 8. Install Terraform
 Write-Output "Installing Terraform..."
 scoop install terraform
-terraform --version
-Write-Output "Terraform installed successfully."
+if (-not (Get-Command terraform -ErrorAction SilentlyContinue)) {
+    Write-Output "terraform not found...."
+
+} else {
+    Write-Output "Terraform installed successfully."
+    terraform --version
+}
+
 
 Write-Output "All installations completed successfully."
